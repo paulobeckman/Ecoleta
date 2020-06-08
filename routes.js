5edb17d8874a76915f934735bda597cebd2f3450
Paulo Beckman <beckmam.paulo@hotmail.com>
2020-06-08 20:33:57 -0300

    third

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = express.Router()
 
+const db = require("./database/db")
+
 routes.get("/", function(req, res){
     return res.render("home")
 })
@@ -8,8 +10,63 @@ routes.get("/", function(req, res){
 routes.get("/register", function(req, res){
     return res.render("register")
 })
+
+routes.post("/savepoint", (req, res) => {
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items,
+    ]
+    function afterInsertData(err){
+        if(err) {
+             console.log(err)
+             return res.send("Erro o cadastro!")
+        }
+
+        console.log ("Cadastro com sucesso")
+        console.log(this)
+
+        return res.render("register", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
+
 routes.get("/list", function(req, res){
-    return res.render("list")
+
+    const list = req.query.search
+
+    if(list == ""){
+        return res.render("list", {total: 0})
+    }
+    
+    db.all(`SELECT * FROM places WHERE city LIKE '%${list}%'`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+        
+        return res.render("list", {places: rows, total})
+    })
 })
 
 module.exports = routes
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const routes = require("./routes")
 const server = express()
 
 server.use(express.static('public'))
+server.use(express.urlencoded({extended:true}))
+
 server.use(routes)
 
 server.set("view engine", "njk")
