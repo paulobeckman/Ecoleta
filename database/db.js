5c398863c0383c4d68707adbeaf29c2b70e75814
Paulo Beckman <beckmam.paulo@hotmail.com>
2020-06-09 22:19:32 -0300

    bedroom commit

--- database/db.js
+++ database/db.js
@@ -54,7 +54,7 @@ module.exports = db
 
 
 //     //deletar registro
-//     // db.run (`DELETE FROM places WHERE id = ?`, [5], function(err) {
+//     // db.run (`DELETE FROM places WHERE id = ?`, [10], function(err) {
 //     //     if(err) {
 //     //         return console.log(err)
 //     //     }
